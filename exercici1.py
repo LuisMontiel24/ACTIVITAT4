977c66707dc12df286d9cf5fa30433b0b2fed373
AbdelazizKafi <2024_abdelaziz.kafi@iticbcn.cat>
2024-10-27 15:33:31 +0100

    Add files via upload
    commit entrega abdelaziz kafi

--- exercici1.py
+++ exercici1.py
@@ -1,16 +1,17 @@
 def crear_matriu_diagonal():
-    # Crear una matriu 7x7 amb 0s i només valors ascendents a la diagonal
+    # Creo una matriu de 7x7 amb 0s i només permetre valors ascendents en diagonal
     matriu = [[0 for _ in range(7)] for _ in range(7)]
     for i in range(7):
-        matriu[i][i] = i * 7  # Els valors seran de 0 a 49 de 7 en 7
+        # Els valors que vull que surtin han d'arribar al 49 i 7 es el seu multiple
+        matriu[i][i] = i * 7 
     return matriu
 
 def guardar_matriu(matriu, filename):
-    # Guardar la matriu en un fitxer de text
+    # Guardo el resultat de la matriu en un fitxer txt que serà creat al executar el programa
     with open(filename, 'w') as file:
         for row in matriu:
             file.write(" ".join(map(str, row)) + "\n")
 
-# Executa la funció i guarda la matriu
+# Executo la funció i guardo la matriu
 matriu = crear_matriu_diagonal()
 guardar_matriu(matriu, "exercici1.txt")
